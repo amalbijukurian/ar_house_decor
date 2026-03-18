ec5b82b868eed88b63ac53abf285b55ce0ebda92
Adarsh-M-Nair <adarshvaikhary@gmail.com>
2026-03-18 12:06:17 +0530

    New Commit

--- ar_room_decor/Backend/test_diffusion.py
+++
@@ -1,9 +0,0 @@
-from models.light_diffusion_model import generate_wall
-
-generate_wall(
-    "wall.jpeg",
-    "a living room wall painted light blue",
-    "output.png"
-)
-
-print("Image generated")
